3fe77261168b8218113f7349e9b247d9db273ee8
Student apna collge <cnaik7940@gmail.com>
2026-03-23 15:09:19 +0530

    new commit

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -6,7 +6,21 @@ const AppContext = createContext(initialState)
 
 export const AppProvider = ({children} : {childeren: React.ReactNode})=>{
 
-    return <AppContext.Provider>
+    const navigate = useNavigate()
+    const [user, setUser] = useState<User></User>(null)
+    const [isUserFetched, setIsUserFetched] = useState(false)
+    const [onboardingCompleted, setonboardingCompleted] = useState(false)
+    const [allFoodLogs, setAllFoodlogs] = useState<FoodEntry[]>([])
+    const [allActivityLogs, setAllActivitylogs] = useState<ActivityEntry[]>([])
+
+    const signup = async 
+
+    const value = {}
+
+    return <AppContext.Provider value={value}>
+         {children}
 
     </AppContext.Provider>
 }
+
+export const useAppContext = ()=> useContext(AppContext)
